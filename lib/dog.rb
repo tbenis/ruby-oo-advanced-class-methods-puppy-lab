661056db9b8495b3dbea6aa6b9fb9c4e1ea4e2ae
Benis Tambe <will.jenifer@yahoo.com>
2021-02-03 20:20:36 -0500

    commit tree 7c686c3a464c254e5a0c23b6c5651f1c6ec40de2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+    def self.print_all
+        @@all.each {|x| puts x.name}
+        
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+end
+
